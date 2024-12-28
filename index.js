a8cc589b729490fe4b692f4bf3edaef7a9259c7a
surayaahlam <surayaahlam@gmail.com>
2024-12-28 09:18:07 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-// const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const corsOptions = {
-  origin: ['http://localhost:5173'],
-  credentials: true,
-  optionalSuccessStatus: 200,
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionalSuccessStatus: 200,
 }
 
 // middleware
 app.use(cors(corsOptions))
 app.use(express.json())
-// app.use(cookieParser())
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bo1l9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,6 +32,19 @@ const client = new MongoClient(uri, {
     }
 });
 
+// verifyToken
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    if (!token) return res.status(401).send({ message: 'unauthorized access' })
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+    })
+    next()
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -42,6 +56,34 @@ async function run() {
         const database = client.db('raceConnectDB');
         const marathonCollection = database.collection('marathons');
         const registrationCollection = database.collection('registrations');
+
+        // generate jwt
+        app.post('/jwt', async (req, res) => {
+            const email = req.body
+            // create token
+            const token = jwt.sign(email, process.env.SECRET_KEY, {
+                expiresIn: '365d', // 1 year
+            })
+            console.log(token)
+            res
+                .cookie('token', token, {
+                    httpOnly: true, // Prevent client-side access
+                    secure: process.env.NODE_ENV === 'production', // Secure flag in production
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+
+        // logout || clear cookie from browser
+        app.get('/logout', async (req, res) => {
+            res
+                .clearCookie('token', {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
 
         // marathons related API
         app.get('/marathons', async (req, res) => {
@@ -90,6 +132,14 @@ async function run() {
         // registrations related API
         app.post('/registrations', async (req, res) => {
             const registration = req.body;
+            // if a user registered in this marathon
+            const query = { email: registration.email, marathonTitle: registration.marathonTitle }
+            const alreadyExist = await registrationCollection.findOne(query)
+            console.log('If already exist-->', alreadyExist)
+            if (alreadyExist)
+                return res
+                    .status(400)
+                    .send('You have already registered for this marathon!')
             const result = await registrationCollection.insertOne(registration);
             res.send(result);
         });
